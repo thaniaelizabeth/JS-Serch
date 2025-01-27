7368277c38299ddae31d224f1dd9f473c6e123bc
Thania Arredondo <elizabethania.te@gmail.com>
2025-01-27 13:50:53 -0600

    feat: conversiones explícitas de string y number

--- js-01-data_types/src/js/main.js
+++ js-01-data_types/src/js/main.js
@@ -127,3 +127,70 @@ console.log( typeof isActive ); // boolean
 
 console.log (typeof (3>5 || 5>3)); // 'boolean'
 
+// --------------------------------------------------
+
+/* 
+Conversión explícita de datos (coerción de tipo)
+En ocasiones en español le llaman encasillamiento y en inglés type casting
+
+El type casting es el acto de convertir variables de un tipo de dato a otro. 
+Esto puede ser implícito (automático) o  explícito (forzado por el desarrollador).
+
+*/
+
+// Conversión implícita
+const resultado = "5" + 2; // 52 Número convertido a string|
+console.log(resultado);
+const multiplicacion = "5" * 3; // 15 El string convertido a number
+console.log(multiplicacion)
+const division = "two" * 6; // NaN
+console.log(division)
+
+// Conversión explícita a Strings
+const edadMascota = 10;
+const edadMascotaString = String(edadMascota);
+console.log(`Mi mascota tiene ${edadMascotaString} años`);
+console.log(`Mi mascota tiene ${edadMascota} años`);
+
+console.log( String ( true ) ); // "true"
+console.log( String ( null ) ); // "null"
+console.log( String ( [] ) ); // empty Array
+console.log( String ( undefined ) ); // "undefined"
+console.log( String ( [2,3,4,5,null,3] ) ); // "2,3,4,5,,3"
+console.log( String ( {} ) ); // [object Object]
+console.log( String ( {name:"Thania", active: true} ) ); // [object Object]
+// Para los objetos se recomienda usar el método JSON.stringyfy( object )
+console.log( JSON.stringify( { name:"Thania", active: true } ) ); // {"name":"Thania","active":true}
+
+// Conversión explícita a number
+const costo = "100";
+const costoConIVA = costo * 1.16; // 116.0 conversión implícita de string a number
+console.log( costoConIVA );
+const precioCroquetas = "2000";
+const precioJabon = "300.50";
+const precioVitaminas = "600.30";
+const costoTotal = precioCroquetas + precioJabon + precioVitaminas;
+console.log( costoTotal ); // 2000300.50600.30
+/* 
+Number() vs parseInt 
+- Number convierte enteros y decimales. Y devuelve NaN si la cadena contiene algún caracter no numérico.
+- parseInt convierte solo la parte entera
+- parseFloat convierte la parte entera y decimales
+- parseFloat y parseInt realizan la conversión hasta encontrar un caracter no numérico
+*/
+
+console.log( Number("68.58") ); // 68.58
+console.log( parseInt("68.58") ); // 68
+console.log( parseFloat("68.58") ); // 68.58
+
+console.log( Number("68.58 dolares") ); // NaN
+console.log( parseInt("68.58 dolares") ); // 68
+console.log( parseFloat("68.58 dolares ") ); // 68.58
+
+console.log( Number("$68.58 dolares") ); // NaN
+console.log( parseInt("$68.58 dolares") ); // NaN
+console.log( parseFloat("$68.58 dolares ") ); // NaN
+
+
+
+
